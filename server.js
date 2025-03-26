bd9f902093140c23c693877148d87d3addcad557
prajjwalbasnet <prajjwal302@gmail.com>
2025-03-27 04:02:44 +1100

    Initial setup

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import cors from 'cors'
+import 'dotenv/config'
+import connectDB from './config/mongodb.js'
+import connectCloudinary from './config/cloudinary.js'
+
+
+const app = express()
+const port = process.env.PORT || 4000
+connectDB()
+connectCloudinary
+
+// middlewares
+app.use(express.json())
+app.use(cors())
+
+// app.get('/', (req,res)=> {
+//     res.send("API Working")
+// })
+
+app.listen(port, ()=> {
+    console.log(`server started on ${port}.`)
+})
