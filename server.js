28b639d779cfc63951bf7f527b499c50f04cb593
prajjwalbasnet <prajjwal302@gmail.com>
2025-04-20 21:48:32 +1000

    cors url updated

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ connectCloudinary();
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
   })
